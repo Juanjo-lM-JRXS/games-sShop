5577cbb801818afeb2667b883c060961b41283fe
Juanjo-LM-JRXS <104702305+Juanjo-lM-JRXS@users.noreply.github.com>
2024-12-02 15:57:29 -0500

    entreaga 4
    e

--- src/main/java/co/edu/ue/dao/OrdenesdetallesDao.java
+++ src/main/java/co/edu/ue/dao/OrdenesdetallesDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,28 +12,32 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.edu.ue.entity.Ordenesdetalles;
+import co.edu.ue.jpa.IOrdenJpa;
 
 @Repository
 public class OrdenesdetallesDao implements IOrdenesdetallesDao{
 
-	@Override
+	@Autowired
+	IOrdendetallesJpa jpa;
+	
+	@Transactional
 	public List<Ordenesdetalles> guardarDetalleOrden(Ordenesdetalles ordenesdetalles) {
-		// TODO Auto-generated method stub
-		return null;
+	    jpa.save(ordenesdetalles);
+	    return listaOrdenesDetallesCompleta();
 	}
+
 
 	@Override
 	public Ordenesdetalles actualizarDetalleOrden(Ordenesdetalles ordenesdetalles) {
-		// TODO Auto-generated method stub
-		return null;
+		return jpa.save(ordenesdetalles);
 	}
 
 	@Override
 	public List<Ordenesdetalles> listaOrdenesDetallesCompleta() {
-		// TODO Auto-generated method stub
-		return null;
+		return jpa.findAll();
 	}
 
 	@Override
